2b728ad81f0291ce1d775c251ccc25e44ebacd53
bluebird182 <eross.eniko2000@gmail.com>
2024-03-12 02:14:52 +0100

    Add explore

--- app/src/main/java/es/uc3m/android/traveltales/MainActivity.kt
+++ app/src/main/java/es/uc3m/android/traveltales/MainActivity.kt
@@ -1,43 +1,15 @@
 package es.uc3m.android.traveltales
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import es.uc3m.android.traveltales.ui.theme.TravelTalesTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TravelTalesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        val intent = Intent(this, Explore::class.java)
+        startActivity(intent)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TravelTalesTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/es/uc3m/android/traveltales/Profile.kt
+++ app/src/main/java/es/uc3m/android/traveltales/Profile.kt
@@ -2,13 +2,18 @@ package es.uc3m.android.traveltales
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import es.uc3m.android.traveltales.databinding.ActivityProfileBinding
-
 class Profile : Activity() {
     private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener {
+            /*TODO*/
+        }
+
     }
 }
