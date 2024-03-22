4d89562ad0a31bea40934e90c7e31343a7c8f0b1
Medafoui <medafif26@gmail.com>
2024-03-22 13:42:58 +0100

    ui.theme deleted

--- app/src/main/java/es/uc3m/android/traveltales/MainActivity.kt
+++ app/src/main/java/es/uc3m/android/traveltales/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import es.uc3m.android.traveltales.ui.theme.TravelTalesTheme
 import android.content.Intent
 import android.widget.Button
 import androidx.fragment.app.Fragment
